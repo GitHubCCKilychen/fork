b141b9f2ea5d5667400450c44524c5db557e7ae2
GitHubCCKilychen <1300925306@qq.com>
2016-03-31 14:32:45 +0800

    Release 1.0.0.

---
+++ WZMarqueeViewDemo/WZMarqueeViewDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  WZMarqueeViewDemo
+//
+//  Created by 刘晓雪 on 16/3/31.
+//  Copyright © 2016年 haokewangluo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ WZMarqueeViewDemo/WZMarqueeViewDemo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  WZMarqueeViewDemo
+//
+//  Created by 刘晓雪 on 16/3/31.
+//  Copyright © 2016年 haokewangluo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
